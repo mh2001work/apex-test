7dad475d27f314c184d0eac60cb7760122905ecb
mh2001work <mh2001.work@gmail.com>
2025-09-02 15:14:20 +0100

    APEX Serverside export

---
+++ apps/f4000/application/shared_components/security/authorizations/flow_restful.sql
@@ -0,0 +1,27 @@
+prompt --application/shared_components/security/authorizations/flow_restful
+begin
+--   Manifest
+--     SECURITY SCHEME: Flow: RESTFUL
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2099.99.99'
+,p_release=>'25.1.0'
+,p_default_workspace_id=>10
+,p_default_application_id=>4000
+,p_default_id_offset=>0
+,p_default_owner=>'APEX_250100'
+);
+wwv_flow_imp_shared.create_security_scheme(
+ p_id=>wwv_flow_imp.id(802892920094782611)
+,p_name=>'Flow: RESTFUL'
+,p_static_id=>'flow-restful'
+,p_scheme_type=>'PLUGIN_COM.ORACLE.APEX.DEVELOPER_AUTHORIZATION'
+,p_attribute_01=>'RESTFUL'
+,p_error_message=>'No privilege for attempted operation'
+,p_reference_id=>wwv_imp_util.get_subscription_id(786519856057734816,3060,'flow-restful',4999)
+,p_version_scn=>'SH256:-7nJWaVUxPBE6M49Aey5AKrDDp9N37OxDHJbia8pwSo'
+,p_caching=>'BY_USER_BY_SESSION'
+);
+wwv_flow_imp.component_end;
+end;
+/
