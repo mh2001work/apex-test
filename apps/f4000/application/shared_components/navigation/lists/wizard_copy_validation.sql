fad18229650556b90e68f6d31e6655b8a774bd98
mh2001work <mh2001.work@gmail.com>
2025-09-02 15:13:31 +0100

    APEX Serverside export

---
+++ apps/f4000/application/shared_components/navigation/lists/wizard_copy_validation.sql
@@ -0,0 +1,22 @@
+prompt --application/shared_components/navigation/lists/wizard_copy_validation
+begin
+--   Manifest
+--     LIST: Wizard Copy Validation
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2099.99.99'
+,p_release=>'25.1.0'
+,p_default_workspace_id=>10
+,p_default_application_id=>4000
+,p_default_id_offset=>0
+,p_default_owner=>'APEX_250100'
+);
+wwv_flow_imp_shared.create_list(
+ p_id=>wwv_flow_imp.id(228167614670175482)
+,p_name=>'Wizard Copy Validation'
+,p_static_id=>'Wizard-Copy-Validation'
+,p_version_scn=>'SH256:lt2nFuKgsnDck7nKaVVvWUEeJxazawmCbsKR6Ro3fNg'
+);
+wwv_flow_imp.component_end;
+end;
+/
