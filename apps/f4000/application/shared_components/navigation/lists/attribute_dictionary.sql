f008e5a58fbea06824d5ed9a92126a06c69c8d05
mh2001work <mh2001.work@gmail.com>
2025-09-02 15:13:40 +0100

    APEX Serverside export

---
+++ apps/f4000/application/shared_components/navigation/lists/attribute_dictionary.sql
@@ -0,0 +1,22 @@
+prompt --application/shared_components/navigation/lists/attribute_dictionary
+begin
+--   Manifest
+--     LIST: Attribute Dictionary
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2099.99.99'
+,p_release=>'25.1.0'
+,p_default_workspace_id=>10
+,p_default_application_id=>4000
+,p_default_id_offset=>0
+,p_default_owner=>'APEX_250100'
+);
+wwv_flow_imp_shared.create_list(
+ p_id=>wwv_flow_imp.id(445192614743849621)
+,p_name=>'Attribute Dictionary'
+,p_static_id=>'Attribute-Dictionary'
+,p_version_scn=>'SH256:ICZRE8hIma3y3O1_HFExcZc_ovbORm8eJh_u3R3o6z4'
+);
+wwv_flow_imp.component_end;
+end;
+/
