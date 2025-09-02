8536dbccbf1784aff6ad3c62132f618fe0f0a928
mh2001work <mh2001.work@gmail.com>
2025-09-02 15:11:00 +0100

    APEX Serverside export

---
+++ apps/f4000/application/shared_components/navigation/lists/build_option_tasks.sql
@@ -0,0 +1,46 @@
+prompt --application/shared_components/navigation/lists/build_option_tasks
+begin
+--   Manifest
+--     LIST: Build Option Tasks
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2099.99.99'
+,p_release=>'25.1.0'
+,p_default_workspace_id=>10
+,p_default_application_id=>4000
+,p_default_id_offset=>0
+,p_default_owner=>'APEX_250100'
+);
+wwv_flow_imp_shared.create_list(
+ p_id=>wwv_flow_imp.id(4232641859826655)
+,p_name=>'Build Option Tasks'
+,p_static_id=>'Build-Option-Tasks'
+,p_version_scn=>'SH256:DTolIrgP2FnI-J0dDOddOqDE1tIT06oPRNYTTl1dMKI'
+);
+wwv_flow_imp_shared.create_list_item(
+ p_id=>wwv_flow_imp.id(4233474343841029)
+,p_list_item_display_sequence=>10
+,p_list_item_link_text=>'Copy from another app'
+,p_static_id=>'Copy-from-another-app'
+,p_list_item_link_target=>'f?p=&APP_ID.:4912:&SESSION.::&DEBUG.:4912:P4912_COPY_SOURCE:OTHER:'
+,p_list_item_current_type=>'TARGET_PAGE'
+);
+wwv_flow_imp_shared.create_list_item(
+ p_id=>wwv_flow_imp.id(6345111830118127)
+,p_list_item_display_sequence=>20
+,p_list_item_link_text=>'Refresh Subscriptions'
+,p_static_id=>'Refresh-Subscriptions'
+,p_list_item_link_target=>'f?p=&APP_ID.:6:&SESSION.::&DEBUG.:6:P6_COMPONENT_TYPE:BUILD_OPTION:'
+,p_list_item_disp_cond_type=>'EXISTS'
+,p_list_item_disp_condition=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'select *',
+'  from wwv_flow_patches',
+' where :APP_PAGE_ID      = 4910',
+'   and reference_id      is not null',
+'   and flow_id           = :FB_FLOW_ID',
+'   and security_group_id = :FLOW_SECURITY_GROUP_ID'))
+,p_list_item_current_type=>'TARGET_PAGE'
+);
+wwv_flow_imp.component_end;
+end;
+/
