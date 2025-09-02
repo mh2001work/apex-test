810ebbfbe2ee6bac53ad695ee05e807bcf8ab233
mh2001work <mh2001.work@gmail.com>
2025-09-02 15:15:05 +0100

    APEX Serverside export

---
+++ apps/f4000/application/shared_components/logic/application_items/wizard_entry_page.sql
@@ -0,0 +1,22 @@
+prompt --application/shared_components/logic/application_items/wizard_entry_page
+begin
+--   Manifest
+--     APPLICATION ITEM: WIZARD_ENTRY_PAGE
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2099.99.99'
+,p_release=>'25.1.0'
+,p_default_workspace_id=>10
+,p_default_application_id=>4000
+,p_default_id_offset=>0
+,p_default_owner=>'APEX_250100'
+);
+wwv_flow_imp_shared.create_flow_item(
+ p_id=>wwv_flow_imp.id(106733613326936844)
+,p_name=>'WIZARD_ENTRY_PAGE'
+,p_escape_on_http_output=>'N'
+,p_version_scn=>'SH256:hKk_RnjAMn2goTfmYun3XAADm5ABLd9a4aZJxyYZrGw'
+);
+wwv_flow_imp.component_end;
+end;
+/
