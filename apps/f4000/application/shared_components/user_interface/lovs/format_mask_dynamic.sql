e6ce52c86cfb31b2ab51fd05625da3f487cf95dc
mh2001work <mh2001.work@gmail.com>
2025-09-02 15:19:04 +0100

    APEX Serverside export

---
+++ apps/f4000/application/shared_components/user_interface/lovs/format_mask_dynamic.sql
@@ -0,0 +1,27 @@
+prompt --application/shared_components/user_interface/lovs/format_mask_dynamic
+begin
+--   Manifest
+--     FORMAT_MASK_DYNAMIC
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2099.99.99'
+,p_release=>'25.1.0'
+,p_default_workspace_id=>10
+,p_default_application_id=>4000
+,p_default_id_offset=>0
+,p_default_owner=>'APEX_250100'
+);
+wwv_flow_imp_shared.create_list_of_values(
+ p_id=>wwv_flow_imp.id(44798420131812391)
+,p_lov_name=>'FORMAT_MASK_DYNAMIC'
+,p_static_id=>'format-mask-dynamic'
+,p_lov_query=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'select d, r ',
+'  from wwv_flow_format_masks'))
+,p_source_type=>'LEGACY_SQL'
+,p_location=>'LOCAL'
+,p_version_scn=>'SH256:DILe43LbcySSiNU0DsLCnXEW52W6lI_zVqHixQEQzms'
+);
+wwv_flow_imp.component_end;
+end;
+/
