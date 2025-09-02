f6b15b38b7d20d689723ef936fc3bc041f969ade
mh2001work <mh2001.work@gmail.com>
2025-09-02 15:14:52 +0100

    APEX Serverside export

---
+++ apps/f4000/application/shared_components/logic/application_items/fb_theme_id.sql
@@ -0,0 +1,22 @@
+prompt --application/shared_components/logic/application_items/fb_theme_id
+begin
+--   Manifest
+--     APPLICATION ITEM: FB_THEME_ID
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2099.99.99'
+,p_release=>'25.1.0'
+,p_default_workspace_id=>10
+,p_default_application_id=>4000
+,p_default_id_offset=>0
+,p_default_owner=>'APEX_250100'
+);
+wwv_flow_imp_shared.create_flow_item(
+ p_id=>wwv_flow_imp.id(146658914296443870)
+,p_name=>'FB_THEME_ID'
+,p_escape_on_http_output=>'N'
+,p_version_scn=>'SH256:ExPmMDXNl-Q5OZd94_EHVwP1gn4QJ09rrNtg0IDtUs0'
+);
+wwv_flow_imp.component_end;
+end;
+/
